70b6e522e6c04b25fa7a8daf5632e128f08027a9
VavakinV <vladvavakin@gmail.com>
2025-09-15 18:11:03 +0300

    Lab2

---
+++ Lab2/src/Main.java
@@ -0,0 +1,56 @@
+import mpi.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MPI.Init(args);
+
+        int myrank = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
+        int TAG = 0;
+
+        int[] buf = new int[1];
+        buf[0] = myrank;
+
+        int[] received_message = new int[1];
+
+        // 1. Блокирующий режим
+//        if (myrank == 0) {
+//            MPI.COMM_WORLD.Sendrecv(buf, 0, 1, MPI.INT, myrank + 1, TAG, received_message, 0, 1, MPI.INT, size - 1, TAG);
+//            buf[0] += received_message[0];
+//            System.out.println("Rank sum from process " + myrank + ": " + buf[0]);
+//        } else if (myrank == (size - 1)){
+//            MPI.COMM_WORLD.Recv(received_message, 0, 1, MPI.INT, myrank - 1, TAG);
+//            System.out.println("Process " + myrank + " received message: " + received_message[0]);
+//            buf[0] += received_message[0];
+//            MPI.COMM_WORLD.Send(buf, 0, 1, MPI.INT, 0, TAG);
+//        } else {
+//            MPI.COMM_WORLD.Recv(received_message, 0, 1, MPI.INT, myrank - 1, TAG);
+//            System.out.println("Process " + myrank + " received message: " + received_message[0]);
+//            buf[0] += received_message[0];
+//            MPI.COMM_WORLD.Send(buf, 0, 1, MPI.INT, myrank + 1, TAG);
+//        }
+
+        // 2. Неблокирующий режим
+        if (myrank == 0) {
+            MPI.COMM_WORLD.Isend(buf, 0, 1, MPI.INT, myrank + 1, TAG);
+            Request recvReq = MPI.COMM_WORLD.Irecv(received_message, 0, 1, MPI.INT, size - 1, TAG);
+            recvReq.Wait();
+            buf[0] += received_message[0];
+            System.out.println("Rank sum from process " + myrank + ": " + buf[0]);
+
+        } else if (myrank == (size - 1)) {
+            Request recvReq = MPI.COMM_WORLD.Irecv(received_message, 0, 1, MPI.INT, myrank - 1, TAG);
+            recvReq.Wait();
+            System.out.println("Process " + myrank + " received message: " + received_message[0]);
+            buf[0] += received_message[0];
+            MPI.COMM_WORLD.Isend(buf, 0, 1, MPI.INT, 0, TAG);
+        } else {
+            Request recvReq = MPI.COMM_WORLD.Irecv(received_message, 0, 1, MPI.INT, myrank - 1, TAG);
+            recvReq.Wait();
+            System.out.println("Process " + myrank + " received message: " + received_message[0]);
+            buf[0] += received_message[0];
+            MPI.COMM_WORLD.Isend(buf, 0, 1, MPI.INT, myrank + 1, TAG);
+        }
+        MPI.Finalize();
+    }
+}
