369861b9fe9f5f75a110df8eab49dc530d2b8b14
VavakinV <vladvavakin@gmail.com>
2025-10-12 20:04:56 +0300

    Lab4 v1.0

---
+++ Lab4/run_tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+MPJ_HOME="/C/MPJ/mpj-v0_44"   
+MPJ_JAR="$MPJ_HOME/lib/mpj.jar"
+CLASS_NAME="Main"
+
+javac -cp ".:$MPJ_JAR" -d . src/$CLASS_NAME.java
+if [ $? -ne 0 ]; then
+    echo "Ошибка компиляции."
+    exit 1
+fi
+
+rm -f results.csv
+
+sizes=(100000 1000000 10000000)
+procs=(10)
+
+echo "=== Запуск тестов ==="
+for p in "${procs[@]}"; do
+    for n in "${sizes[@]}"; do
+        echo "Процессы: $p | N: $n"
+        mpjrun.sh -np $p $CLASS_NAME $n
+        if [ $? -ne 0 ]; then
+            echo "Ошибка запуска: процессы=$p, N=$n"
+        fi
+    done
+done
+
+echo "Тесты завершены. Результаты сохранены в results.csv"
